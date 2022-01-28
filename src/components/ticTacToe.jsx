13ecfac1a8343c8a733f6ab6faf16ebab2c1a52f
DannyTheOG <nii.ayi.97.na@gmail.com>
2022-01-28 11:24:05 +0000

    initial commit

---
+++ src/components/ticTacToe.jsx
@@ -0,0 +1,14 @@
+import {React,  Component } from "react";
+
+class TicTacToe extends Component {
+     render(){
+          return (
+               <div>
+                    <h1>Happy Coding!</h1>
+                    <button>Retry</button>
+               </div>
+          )
+     }
+}
+
+export default TicTacToe;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TicTacToe from './components/ticTacToe';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <TicTacToe />,
   document.getElementById('root')
 );
 
