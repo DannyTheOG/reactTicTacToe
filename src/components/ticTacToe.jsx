4ac242ba36964a04c6ee1c7d298d41b327e4d5fe
DannyTheOG <nii.ayi.97.na@gmail.com>
2022-06-12 18:46:11 +0000

    updates

--- src/components/ticTacToe.jsx
+++ src/components/ticTacToe.jsx
@@ -1,34 +1,8 @@
-import React,  {Component} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import App from '../dashboard'
 
-class TicTacToe extends Component {
-     state = {
-          count: new Date().toLocaleTimeString(),
-     };
-
-     styles = {
-          fontSize: 20,
-          fontWeight: "bold"
-     };
-
-     // componentDidMount() {
-     //      setInterval(() => {
-     //           this.setState({count: new Date().toLocaleTimeString()});
-     //      }, 1000);
-     // }
-    
-     render() {
-          return (
-               <div>
-                    <span style={this.styles} className="m-2">{new Date().toLocaleTimeString()}</span>
-                    {/* <button className="btn btn-primary">Increment</button> */}
-               </div>
-          )
-     };
-
-     
+export default function TicTacToe() {
+     <App />
 }
 
 
-
-export default TicTacToe;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import TicTacToe from './components/ticTacToe';
+import React from "react";
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-
-const element = <Welcome name="Sara" />;
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
-
-
-// function tick() {
-//   ReactDOM.render(
-//     <TicTacToe />,
-//   document.getElementById('root')
-//   );
-// }
-
-// setInterval(tick, 1000);
-
-reportWebVitals();
+export default function App() {
+     return (
+       <div >
+         happy coding
+       </div>
+     );
+   }
+   
